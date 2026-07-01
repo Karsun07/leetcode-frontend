e79cafce6cbdff1727cad7671d85f0c569d6f392
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-07-01 11:44:44 +0530

    validation applied using zod and react hook form used connected by zodResolver

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter,Routes,Route } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App(){
   return (<>
-    Hello
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+      </Routes>
+      </BrowserRouter>
+
       </>)
 }
 export default App;
